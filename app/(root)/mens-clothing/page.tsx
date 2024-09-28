df1b1fa469a66f4d19dcd6e4fc4e35db97d9940d
VladBoguslavskiy <152431136+VladBoguslavskiy@users.noreply.github.com>
2024-09-28 13:33:10 +0300

    Update page.tsx

--- app/(root)/mens-clothing/page.tsx
+++ app/(root)/mens-clothing/page.tsx
@@ -1,25 +1,32 @@
+import { useEffect, useState } from "react";
 import ProductCard from "@/components/ProductCard";
 import { getProducts } from "@/lib/actions/actions";
 
-const MensClothing = async () => {
-  // Отримуємо всі товари для чоловічого одягу
-  const mensClothingProducts = await getProducts(); // Передайте тег "чоловічий"
+const MensClothing = () => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+  
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const allProducts = await getProducts();
+      const filteredProducts = allProducts.filter((product: ProductType) =>
+        product.tags.includes("Чоловічий")
+      );
+      setProducts(filteredProducts);
+    };
 
-  // Фільтруємо товари за тегом "чоловічий"
-  const filteredProducts = mensClothingProducts.filter((product: ProductType) =>
-    product.tags.includes("Чоловічий") // Перевіряємо, чи містить масив тегів "чоловічий"
-  );
+    fetchProducts();
+  }, []);
 
   return (
     <div className="flex flex-col items-center px-10 py-5 max-md:px-3">
       <p className="text-heading3-bold">Чоловічий одяг</p>
       <div className="flex flex-wrap gap-16 mx-auto mt-8">
-        {filteredProducts.length > 0 ? (
-          filteredProducts.map((product: ProductType) => (
+        {products.length > 0 ? (
+          products.map((product: ProductType) => (
             <ProductCard key={product._id} product={product} />
           ))
         ) : (
-          <p>Немає товарів у цій категорії.</p> // Показати повідомлення, якщо немає товарів
+          <p>Немає товарів у цій категорії.</p>
         )}
       </div>
     </div>
